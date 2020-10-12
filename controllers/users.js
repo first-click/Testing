25ac88cb5bd23af9ad85b5b2cb631fdd8ec76e38
first-click <hova-cr@web.de>
2020-10-12 13:32:20 +0200

    use prettier to remove unneeded blank lines

--- controllers/users.js
+++ controllers/users.js
@@ -1,42 +1,30 @@
 const { sequelize } = require('../models');
 const User = sequelize.models.User;
 
-
-
 //@desc get all users
 //@route Get /users/
 //@access Public
 exports.getUsers = async (req, res) => {
-
   try {
-
-    const users = await User.findAll()
-    res.json(users)
-
+    const users = await User.findAll();
+    res.json(users);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-}
-
-
+};
 
 //@desc Create new user
 //@route Post /users/
 //@access Public
 exports.createUser = async (req, res) => {
-
   try {
     // Insert into table
     const user = await User.create({
-      name: req.body.name
+      name: req.body.name,
+    });
 
-    })
-
-    res.json(user)
-
+    res.json(user);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-}
+};
