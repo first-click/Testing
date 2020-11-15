3e80d8f54fdc042d3d6db362b1ca70b6576adefe
first-click <hova-cr@web.de>
2020-11-15 08:41:56 +0100

    add tests for user route

--- tests/user.test.js
+++ tests/user.test.js
@@ -8,6 +8,7 @@ const { userOne, userTwo, setUpDatabase } = require('./fixtures/db');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 let token;
+let id;
 beforeAll(setUpDatabase);
 
 test('Should login existing user', async () => {
@@ -18,34 +19,65 @@ test('Should login existing user', async () => {
       password: userTwo.password,
     })
     .expect(200);
-
-  token = JSON.parse(response.text);
+  token = response.body.token;
 });
 
-test('Should create User', async () => {
+test('Should get all Users', async () => {
+  const response = await request(app)
+    .get('/api/v1/users')
+    .set('Authorization', `Bearer ${token}`)
+    .expect(200);
+  expect(response.body[0].username).toBe('testit11');
+  expect(response.body[1].username).toBe('testit12');
+});
+
+test('Should get a single User', async () => {
+  const response = await request(app)
+    .get('/api/v1/users/1')
+    .set('Authorization', `Bearer ${token}`)
+    .expect(200);
+  expect(response.body.username).toBe('testit11');
+});
+
+//@desc Create new user
+//@route POST /api/v1/users
+test('Should create a new User', async () => {
   const response = await request(app)
     .post('/api/v1/users')
-    .set('Authorization', `Bearer ${token.token}`)
+    .set('Authorization', `Bearer ${token}`)
     .send({
-      username: 'user1',
-      email: 'user1@gmx.de',
+      username: 'user3',
+      email: 'user3@gmx.de',
       password: '123456',
-      role: 'user',
+      role: 'admin',
     })
     .expect(200);
-  console.log(response.body);
+  expect(response.body.username).toEqual('user3');
+  id = response.body.id;
 });
 
-// test('Should create User', async () => {
-//   const response = await request(app)
-//     .post('/api/v1/users')
-//     .set('Authorization', `Bearer ${token.token}`)
-//     .send({
-//       username: 'user1',
-//       email: 'user1@gmx.de',
-//       password: '123456',
-//       role: 'user',
-//     })
-//     .expect(200);
-//   console.log(response.body);
-// });
+//@desc Update a user
+//@route PUT /api/v1/users/:id
+test('Should update a User', async () => {
+  const response = await request(app)
+    .put(`/api/v1/users/${id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+      username: 'user4',
+      email: 'user4@gmx.de',
+      password: '123456',
+    })
+    .expect(200);
+  expect(response.body[0]).toEqual(1);
+});
+
+//@desc Delete a user
+//@route DELETE /api/v1/users/:id
+test('Should delete a User', async () => {
+  const response = await request(app)
+    .delete(`/api/v1/users/${id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .send()
+    .expect(200);
+  expect(response.body.success).toEqual(true);
+});
