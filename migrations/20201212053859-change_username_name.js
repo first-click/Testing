f6d0bf4185dbc4b9d164e1e68c1b22f10bbbac0d
jleweli <hova-cr@web.de>
2021-01-08 21:58:26 +0100

    edit migration to improve rollback

--- migrations/20201212053859-change_username_name.js
+++ migrations/20201212053859-change_username_name.js
@@ -1,53 +1,21 @@
 'use strict';
 
 module.exports = {
+  /**
+   * @typedef {import('sequelize').Sequelize} Sequelize
+   * @typedef {import('sequelize').QueryInterface} QueryInterface
+   */
+
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-          len: {
-            args: [1, 20],
-            msg: 'please use the right length',
-          },
-        },
-      },
-      email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      role: {
-        type: Sequelize.STRING,
-        enum: ['user', 'publisher'],
-        default: 'user',
-      },
-      resetPasswordToken: Sequelize.STRING,
-      resetPasswordExpire: Sequelize.DATE,
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+    await queryInterface.renameColumn('users', 'username', 'name');
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
+    await queryInterface.renameColumn('users', 'name', 'username');
   },
 };
