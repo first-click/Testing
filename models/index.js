0e325a4747f04e4034c9ee720d0b8d3035805304
jleweli <hova-cr@web.de>
2021-01-10 00:30:36 +0100

    fix reference to config.username

--- models/index.js
+++ models/index.js
@@ -10,12 +10,14 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+console.log(config);
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
-    config.name,
+    // config.name,
+    config.username, //muss username sein, gemeint ist der DB user
     config.password,
     config
   );
@@ -40,7 +42,6 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
