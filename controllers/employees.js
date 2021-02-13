aa7d9bb725016136c333424d6f59f0dc6aaf37c3
Petra <petrakohler@gmx.de>
2021-02-13 13:41:43 +0100

    cte

--- controllers/employees.js
+++ controllers/employees.js
@@ -29,20 +29,24 @@ const Employee = sequelize.models.employee;
 //   });
 // });
 
-// exports.getEmployees = asyncHandler(async (req, res) => {
-//   const result = await Employee.findAll({
-//     where: {
-//       manager_id: null,
-//     },
-//     include: [
-//       {
-//         model: Employee,
-//         as: 'children',
-//       },
-//     ],
-//   });
-//   res.send(result);
-// });
+exports.getEmployees = asyncHandler(async (req, res) => {
+  const result = await Employee.findAll({
+    where: {
+      manager_id: 1,
+    },
+    include: [
+      {
+        model: Employee,
+        as: 'children',
+      },
+    ],
+  });
+
+  let test = Object.assign({}, result);
+
+  //Object.assign({}, result)
+  res.send(test);
+});
 
 //@desc Get all positions
 //@route GET /api/v1/employees
@@ -67,20 +71,20 @@ const Employee = sequelize.models.employee;
 //@desc Get all positions
 //@route GET /api/v1/employees
 //@access Private/Admin erstmal nicht privat
-exports.getEmployees = asyncHandler(async (req, res) => {
-  const employees = await sequelize.query(`
-  WITH RECURSIVE
-      cte(employee_id, manager_id, level) AS (
-          SELECT employee_id, manager_id, 1 FROM employees
-           WHERE 1 = employees.employee_id
-           UNION
-           SELECT employees.employee_id, employees.manager_id, cte.level+1 FROM employees, cte
-           WHERE employees.manager_id = cte.employee_id AND cte.level+1 <= 3
-      )
-      SELECT employees.name FROM employees, cte
-      WHERE employees.employee_id = cte.employee_id AND cte.level = 3;`);
-  res.status(200).json({
-    success: true,
-    data: employees[0],
-  });
-});
+// exports.getEmployees = asyncHandler(async (req, res) => {
+//   const employees = await sequelize.query(`
+//   WITH RECURSIVE
+//       cte(employee_id, manager_id, level) AS (
+//           SELECT employee_id, manager_id, 1 FROM employees
+//            WHERE 1 = employees.employee_id
+//            UNION
+//            SELECT employees.employee_id, employees.manager_id, cte.level+1 FROM employees, cte
+//            WHERE employees.manager_id = cte.employee_id AND cte.level+1 <= 3
+//       )
+//       SELECT employees.name FROM employees, cte
+//       WHERE employees.employee_id = cte.employee_id AND cte.level = 3;`);
+//   res.status(200).json({
+//     success: true,
+//     data: employees[0],
+//   });
+// });
