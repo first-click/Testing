217e03e3b8807680096349be960b97a069539326
Petra <petrakohler@gmx.de>
2021-02-15 16:42:19 +0100

    recursive cte all manager with level

--- controllers/employees.js
+++ controllers/employees.js
@@ -29,44 +29,56 @@ const Employee = sequelize.models.employee;
 //   });
 // });
 
-exports.getEmployees = asyncHandler(async (req, res) => {
-  const result = await Employee.findAll({
-    where: {
-      manager_id: 2,
-    },
-    include: [
-      {
-        model: Employee,
-        as: 'children',
-      },
-    ],
-  });
+// exports.getEmployees = asyncHandler(async (req, res) => {
+//   const result = await Employee.findAll({
+//     where: {
+//       manager_id: 1,
+//     },
+//     include: [
+//       {
+//         model: Employee,
+//         as: 'children',
+//       },
+//     ],
+//   });
 
-  let test = Object.assign({}, result);
+//   let test = Object.assign({}, result);
 
-  //Object.assign({}, result)
-  res.send(result);
-});
+//   //Object.assign({}, result)
+//   res.send(result);
+// });
 
 //@desc Get all positions
 //@route GET /api/v1/employees
 //@access Private/Admin erstmal nicht privat
-// exports.getEmployees = asyncHandler(async (req, res) => {
-//   const employees = await sequelize.query(`WITH RECURSIVE
-//   cte(employee_id, manager_id) AS (
-//       SELECT employee_id, manager_id FROM employees
-//        WHERE 19 = employees.employee_id
-//        UNION
-//        SELECT employees.employee_id, employees.manager_id FROM employees, cte
-//        WHERE employees.employee_id = cte.manager_id
-//   )
-//   SELECT employees.name FROM employees, cte
-//   WHERE cte.employee_id = employees.employee_id;`);
-//   res.status(200).json({
-//     success: true,
-//     data: employees,
-//   });
-// });
+exports.getEmployees = asyncHandler(async (req, res) => {
+  const employees = await sequelize.query(`With RECURSIVE 
+  EmployeeCTE (employee_id, name, manager_id, level)
+  as
+  (
+  Select employee_id, name, manager_id, 1
+    from employees
+    where manager_id is null
+    
+    union all
+    Select employees.employee_id, employees.name,
+    employees.manager_id, EmployeeCTE.level + 1
+    from employees
+    join EmployeeCTE
+    on employees.manager_id = EmployeeCTE.employee_id
+    
+  )
+  Select EmpCTE.name as Employee,COALESCE(MgrCTE.name,'super boss')  as Manager,
+  EmpCTE.level
+  from EmployeeCTE EmpCTE
+  left join EmployeeCTE MgrCTE
+  on EmpCTE.manager_id = MgrCTE.employee_id
+  `);
+  res.status(200).json({
+    success: true,
+    data: employees,
+  });
+});
 
 //@desc Get all positions
 //@route GET /api/v1/employees
