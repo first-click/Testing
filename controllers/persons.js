1c58fca001f3b6c271b3277cb6231066ab37a041
Petra <petrakohler@gmx.de>
2021-05-28 18:07:47 +0200

    upload works

--- controllers/persons.js
+++ controllers/persons.js
@@ -109,22 +109,9 @@ exports.createPerson = asyncHandler(async (req, res, next) => {
       for (const file of files.file) {
         const pathFile = 'uploads/';
 
-        function getCurrentFilenames() {
-          console.log('Current filenames:');
-          fs.readdirSync(__dirname).forEach((file) => {
-            console.log(file);
-          });
-        }
-
         fs.rename(file.path, path.join(pathFile, file.name), (err) => {
           if (err) {
             return next(new ErrorResponse('Person could not be created', 401));
-          } else {
-            // List all the filenames after renaming
-            console.log('\nFile Renamed\n');
-
-            // List all the filenames after renaming
-            getCurrentFilenames();
           }
         });
 
